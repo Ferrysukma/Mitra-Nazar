d0e7303cc7a589ce6c82a2c2ae636ce20129a8e0
Muhammad Ikram <ikram@neuronworks.co.id>
2020-09-30 14:14:20 +0700

    fix user

--- resources/views/user_list.blade.php
+++ resources/views/user_list.blade.php
@@ -1,81 +1,57 @@
 @extends('layouts.app')
 
 @section('content')
-<!-- Breadcomb area Start-->
-<div class="breadcomb-area">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="breadcomb-list">
-                    <div class="row">
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                            <div class="breadcomb-wp">
-                                <div class="breadcomb-icon">
-                                    <i class="fa fa-user"></i>
-                                </div>
-                                <div class="breadcomb-ctn">
-                                    <h2>{{ __('all.users') }}</h2>
-                                    <p>{{ __('all.desc_users') }}</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-3">
-
-                        </div>
+<!-- Page Heading -->
+<div class="row">
+    <div class="col-lg-12 mb-4">
+        <div class="card border-left-primary shadow h-100">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <h1 class="h3 mb-0 text-gray-800">{{ __('all.users') }}</h1>
+                        <p>{{ __('all.desc_users') }}</p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<!-- Breadcomb area End-->
-<div class="tabs-info-area">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="widget-tabs-int tab-ctm-wp mg-t-30">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="data-table-list">
-                                <div class="table-responsive-sm">
-                                    <div class="row">
-                                        <div class="col-sm-8"></div>
-                                        <div class="col-sm-2"></div>
-                                        <div class="col-sm-2">
-                                            <div class="btn-group" id="grMaps" style="float:left">
-                                                <button type="button" class="btn btn-primary" id="add-mitra"><i class="fa fa-plus"></i> {{ __('all.button.new') }}</button>
-                                                <button class="btn btn-info dropdown-toggle" type="button" id="btnFilter" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                    <i class="fa fa-filter"></i>
-                                                </button>
-                                                <div class="dropdown-menu" aria-labelledby="btnFilter" id="keepOpen">
-                                                    <form action="#">
-                                                        <div class="form-group">
-                                                            <select name="status" id="status" class="form-control" style="width: 100% !important;">
-                                                                <option value="" selected disabled>{{ __('all.placeholder.choose_status') }}</option>
-                                                            </select>
-                                                        </div>
-                                                    </form>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <br>
-                                    <table class="table table-hover table-striped table-bordered" id="table-maps" width="100%">
-                                        <thead>
-                                            <tr>
-                                                <th>No</th>
-                                                <th>{{ __('all.table.date') }}</th>
-                                                <th>{{ __('all.table.prov') }}</th>
-                                                <th>{{ __('all.table.city') }}</th>
-                                                <th>{{ __('all.table.qty') }}</th>
-                                                <th>{{ __('all.table.action') }}</th>
-                                            </tr>
-                                        </thead>
-                                    </table>
-                                </div>
+<!-- Content Row -->
+<div class="row">
+    <div class="col-lg-12 mb-4">
+        <!-- Approach -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <div class="btn-group" role="group" style="float:right">
+                    <button type="button" class="btn btn-primary btn-sm" id="add-user" onclick="showModal('modal-mitra')"><i class="fa fa-plus"></i> {{ __('all.button.new') }}</button>
+                    <button class="btn btn-info btn-sm dropdown-toggle" type="button" id="filterMaps" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onclick="showDropdown('dropMaps')">
+                        <i class="fa fa-filter"></i>
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-right keep-open" aria-labelledby="filterMaps" id="dropMaps" style="width:300px;">
+                        <form id="formFilter" class="px-4 py-3" action="#">
+                            <div class="form-group">
+                                <select name="status" id="status" class="form-control" style="width: 100% !important;">
+                                    <option value="" selected disabled>{{ __('all.placeholder.choose_status') }}</option>
+                                </select>
                             </div>
-                        </div>
+                        </form>
                     </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive-sm">
+                    <table class="table table-hover table-striped table-condensed table-bordered" id="table-maps" width="100%">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>{{ __('all.form.username') }}</th>
+                                <th>{{ __('all.form.email') }}</th>
+                                <th>{{ __('all.form.telp') }}</th>
+                                <th>{{ __('all.table.status') }}</th>
+                                <th>{{ __('all.table.action') }}</th>
+                            </tr>
+                        </thead>
+                    </table>
                 </div>
             </div>
         </div>
@@ -84,10 +60,10 @@
 
 <!-- Start Modal Change Password -->
 <div class="modal fade" id="modal-mitra" role="dialog">
-    <div class="modal-dialog modals-default">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h3 class="modal-title">{{ __('all.add_user') }}</h3>
+                <h3 class="modal-title text-white">{{ __('all.add_user') }}</h3>
                 <hr>
             </div>
             <div class="modal-body">
@@ -95,7 +71,7 @@
                     <div class="form-group row">
                         <label for="old" class="col-sm-3">{{ __('all.form.username') }} <sup class="text-danger">*</sup></label>
                         <div class="col-sm-9">
-                            <input type="text" name="username" id="username" class="form-control readonly" placeholder="{{ __('all.placeholder.username') }}" readonly>
+                            <input type="text" name="username" id="username" class="form-control" placeholder="{{ __('all.placeholder.username') }}">
                         </div>
                     </div>
                     <div class="form-group row">
@@ -120,84 +96,4 @@
     </div>
 </div>
 <!-- End Modal Change Password -->
-<!-- Start Modal Change Password -->
-<div class="modal fade" id="modal-mitra" role="dialog">
-    <div class="modal-dialog modals-default">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h3 class="modal-title">{{ __('all.add_partner') }}</h3>
-                <hr>
-            </div>
-            <div class="modal-body">
-                <center>
-                    <span>{{ __('all.confirm') }} ?</span>
-                    <span>{{ __('all.text_confirm') }}</span>
-                </center>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">{{ __('all.cancel') }}</button>
-                <button type="button" class="btn btn-primary">{{ __('all.yes') }}</button>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- End Modal Change Password -->
-@endsection
-
-@section('script')
-<script>
-    $(document).on('click','#add-mitra', function () {
-        showModal('modal-mitra');
-    });
-
-    $(document).on('click','.remove-mitra', function () {
-        showModal('remove-mitra'); 
-    });
-
-    $('#start_dtm_maps').datepicker({
-        language: 'en',
-        dateFormat: 'dd M yyyy',
-        autoClose: true,
-        onSelect: function(fd, date) {
-            $('#end_dtm_maps').data('datepicker')
-                .update('minDate', date);
-            $('#end_dtm_maps').focus();
-        }
-    });
-
-    $('#end_dtm_maps').datepicker({
-        language: 'en',
-        dateFormat: 'dd M yyyy',
-        autoClose: true,
-        onSelect: function(fd, date) {
-            $('#start_dtm_maps').data('datepicker')
-                .update('maxDate', date);
-        }
-    });
-
-    function initMap() {
-       var myLatlng = {lat: -25.363, lng: 131.044};
-
-        var map6 = new google.maps.Map(document.getElementById('googleMap'), {
-            zoom: 4,
-            center: myLatlng
-        });
-
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            map: map6,
-            title: 'Click to zoom'
-        });
-    }
-
-    initMap();
-
-    $("#btnFilter").on('click',function() {
-        if ($("#grMaps").hasClass('open')) {
-            $("#keepOpen").dropdown('hide');
-        } else {
-            $("#keepOpen").dropdown('show');
-        }
-    });
-</script>
 @endsection
