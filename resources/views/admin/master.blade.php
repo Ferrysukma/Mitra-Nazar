50b5907dfa4701af8a282b5bef7d7202fb3d121f
Muhammad Ikram <ikram@neuronworks.co.id>
2020-10-23 04:05:57 +0700

    update home

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -17,7 +17,7 @@
     </div>
 </div>
 
-<div class="row">
+<div class="row" id="showHomeChart">
     <div class="col-lg-12 mb-4">
         <!-- Content Row -->
         <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
@@ -136,6 +136,118 @@
     </div>
 </div>
 
+<div class="row" id="showHomeDetail">
+    <div class="col-lg-12 mb-4">
+        <!-- Content Row -->
+        <ul class="nav nav-pills mb-3" id="pill-tab" role="tablist">
+            <li class="nav-item">
+                <a class="nav-link active" id="detailchart-tab" data-toggle="tab" href="#chartD" role="tab" aria-controls="chart" aria-selected="true">{{ __('all.chart') }}</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="detailmaps-tab" data-toggle="tab" href="#mapsD" role="tab" aria-controls="maps" aria-selected="false">{{ __('all.maps') }}</a>
+            </li>
+        </ul>
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <div class="btn-group" role="group" style="float:right">
+                    <button class="btn btn-info btn-sm dropdown-toggle" type="button" id="filterDetailChart" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onclick="showDropdown('dropDetailChart')">
+                        <span>{{ __('all.filter') }}</span> <i class="fa fa-filter"></i>
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-right keep-open" aria-labelledby="filterDetailChart" id="dropDetailChart" style="width:300px;">
+                        <form class="px-4 py-3" action="#">
+                            <label for="base">{{ __('all.base') }}</label>
+                            <div class="form-group">
+                                <input type="text" name="start_date" id="start_dtm_detail" class="form-control readonly" placeholder="{{ __('all.start_date') }}" readonly>
+                            </div>
+                            <label for="filter">{{ __('all.filter') }}</label>
+                            <div class="form-group">
+                                <div class="dropdown">
+                                    <input type="hidden" id="idfdProv">
+                                    <input type="text" name="city" class="form-control dropdown-toggle" id="filterDetailProv" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onkeyup="filterDetailPosition('filterDetailProv', 'filter-detailprov', 'showFildetailProv')" placeholder="{{ __('all.table.prov') }}">
+                                    <div class="dropdown-menu filter-detailprov scrollable-menu" id="showFildetailProv">
+                                        <a class="dropdown-item">{{ __('all.datatable.no_data') }}</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="dropdown">
+                                    <input type="text" name="city" class="form-control dropdown-toggle" id="filterDetailCity" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onkeyup="filterDetailPosition('filterDetailCity', 'filter-detailcity', 'showFildetailCity')" placeholder="{{ __('all.table.city') }}">
+                                    <div class="dropdown-menu filter-city scrollable-menu" id="showFildetailCity">
+                                        <a class="dropdown-item">{{ __('all.datatable.no_data') }}</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <select name="tipe" id="tipe" class="form-control select2" onchange="showDataDetail()">
+                                    <option value="">{{ __('all.placeholder.choose_coortype') }}</option>
+                                    <option value="pusat">{{ __('all.checkbox.central') }}</option>
+                                    <option value="provinsi">{{ __('all.checkbox.regional') }}</option>
+                                    <option value="kota">{{ __('all.checkbox.city') }}</option>
+                                    <option value="kecamatan">{{ __('all.checkbox.district') }}</option>
+                                    <option value="desa">{{ __('all.checkbox.village') }}</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <select name="kategori" id="kategori" class="form-control select2" onchange="showDataDetail()"></select>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="tab-content" id="myTabContentD">
+                    <div class="tab-pane fade show active" id="chartD" role="tabpanel" aria-labelledby="detailchart-tab">
+                        <div class="card-title">
+                            <h4>{{ __('all.title_chart') }}</h4>
+                        </div>
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <div style="height:40vh; width:100%" id="divChartD">
+                                <canvas id="myChartD"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="mapsD" role="tabpanel" aria-labelledby="detailmaps-tab">
+                        <div class="card-title">
+                            <h4>{{ __('all.title_maps') }}</h4>
+                        </div>
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <div id="maps-homeMitraD">
+                                <div id="mapsHomeMitraD" style="width:100%;height:50vh"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="col-sm-6 card-title">
+                    <h4>{{ __('all.table_chart') }}</h4>
+                </div>
+            </div>
+            <div class="card-footer">
+                <div class="table-responsive">
+                    <table class="table table-hover table-striped table-condensed table-bordered" id="table-chartD" width="100%">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>{{ __('all.table.date') }}</th>
+                                <th>{{ __('all.table.join_date') }}</th>
+                                <th>{{ __('all.table.partner_id') }}</th>
+                                <th>{{ __('all.table.partner_nm') }}</th>
+                                <th>{{ __('all.table.coordinator_type') }}</th>
+                                <th>{{ __('all.table.prov') }}</th>
+                                <th>{{ __('all.table.city') }}</th>
+                                <th>{{ __('all.table.address') }}</th>
+                                <th>{{ __('all.table.coordinate') }}</th>
+                                <th>{{ __('all.table.downline') }}</th>
+                                <th>{{ __('all.table.status') }}</th>
+                                <th>{{ __('all.table.action') }}</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <!-- Start Modal Change Password -->
 <div class="modal fade" id="disabled-mitra" role="dialog">
     <div class="modal-dialog modals-default">
@@ -222,6 +334,34 @@
         },
     });
 
+    var tables = $('#table-chartD').DataTable({
+        "language" : {
+            "lengthMenu"    : "{{ __('all.datatable.show_entries') }}",
+            "emptyTable"    : "{{ __('all.datatable.no_data') }}",
+            "info"        	: "{{ __('all.datatable.showing_start') }}",
+            "infoFiltered"  : "{{ __('all.datatable.filter') }}",
+            "infoEmpty"     : "{{ __('all.datatable.showing_null') }}",
+            "loadingRecords": "{{ __('all.datatable.load') }}",
+            "processing"    : "{{ __('all.datatable.process') }}",
+            "search"      	: "{{ __('all.datatable.search') }}",
+            "zeroRecords"   : "{{ __('all.datatable.zero') }}",
+            "paginate"      : 
+            {
+                "first"     : "{{ __('all.datatable.first') }}",
+                "last"      : "{{ __('all.datatable.last') }}",
+                "next"      : "{{ __('all.datatable.next') }}",
+                "previous"  : "{{ __('all.datatable.prev') }}",
+            }
+        },
+        "columnDefs"        : [ 
+            { targets: [0], orderable: false, className	: "text-center" },
+            { targets: [12], orderable: false, searchable: false, className	: "text-center" },
+        ],
+        "initComplete"      : function() {
+            $('[data-toggle="tooltip"]').tooltip();
+        },
+    });
+
     function showData() {
         $.ajaxSetup({
             headers: {
@@ -267,6 +407,59 @@
                 $("#table-chart").parent().ploading({action : 'hide'});
             }
         });
+    }
+
+    function showDataDetail() {
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $.ajax({
+            type    : "POST",
+            url     : "{{ route('detailChart') }}",
+            data    : {
+                start   : $('#start_dtm_detail').val(),
+                provinsi: $('#filterDetailProv').val(),
+                kota    : $('#filterDetailCity').val(),
+                tipe    : $('#tipe').val(),
+                kategori: $('#kategori').val(),
+            },
+            dataType: "JSON",
+            beforeSend: function(){
+                tables.clear().draw();
+                $("#table-chartD").parent().ploading({action : 'show'});
+            },
+            success     : function(data){
+                if (data.code == 0) {
+                    list = data.data;
+                    
+                    // if(list.length > 0){
+                    //     $.each(list, function(idx, ref){
+                    //         start = moment.utc(ref.cdate).format('YYYY-M-D');
+                    //         tables.row.add( [
+                    //             idx + 1,
+                    //             ref.cdate,
+                    //             ref.provinsi,
+                    //             ref.kota,
+                    //             ref.total,
+                    //             "<button type='button'class='btn btn-sm btn-info action-detail' onclick='tableDetail(this)' title='{{ __('all.button.detail') }}' data-toggle='tooltip' data-placement='top'><i class='fa fa-eye'></i></button>", 
+                    //         ] ).draw( false );
+                    //     });
+                    // }
+                } 
+            },
+            complete : function () {
+                $("#table-chartD").parent().ploading({action : 'hide'});
+            }
+        });
+    }
+
+    function tableDetail(e) {
+        $('#showHomeChart').hide();
+        $('#showHomeDetail').show();
+        showDataDetail();
     }
 
     function showCategory() {
@@ -521,6 +714,17 @@
         }
     });
 
+    $('#start_dtm_detail').datepicker({
+        language: 'en',
+        dateFormat: 'dd M yyyy',
+        autoClose: true,
+        onSelect: function(fd, date) {
+            showDataDetail();
+            mapsDetail();
+            loadDataChartDetail();
+        }
+    });
+
     $('#end_dtm_chart').datepicker({
         language: 'en',
         dateFormat: 'dd M yyyy',
@@ -533,5 +737,7 @@
                 loadDataChart();
         }
     });
+
+    $('#showHomeDetail').hide();
 </script>
 @endsection
